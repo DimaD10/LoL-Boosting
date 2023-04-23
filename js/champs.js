690e405f93fb31e2597d1f96aa3b152f099007b8
Дима Дзюбенко <dzubenkod7@gmail.com>
2023-04-23 22:31:59 +0300

    champions finished

--- js/champs.js
+++ js/champs.js
@@ -7,55 +7,53 @@ document.querySelector('.champions__add-button').addEventListener('click', e => 
     popup.addEventListener('click', e => {
         if (!e.target.classList.contains(".popup-champ__wrapper") && !e.target.closest('.popup-champ__wrapper')) {
             closePopup();
+            rmIcon(iconsAdded);
         }
     })
     document.addEventListener('keydown', function(event) {
         if (popup.classList.contains('opened')) {
             if (event.key == 'Escape') {
                 closePopup();
+                rmIcon(iconsAdded);
             }
         }
     });
 })
+
 popup.querySelector('.close-btn').addEventListener('click', e => {
     closePopup();
+    rmIcon(iconsAdded);
 })
-
 
 window.onload = () => {
     addIcons();
-    closePopup();
-
-    icons.forEach(el => {
-        el.addEventListener('click', e => {
-            el.classList.toggle('_current');
-        })
-    })
 }
 
 document.querySelector('.popup-champ__confirm').addEventListener('click', e => {
     let listIcons = document.querySelector('.champions__list');
     let iconsAdded = listIcons.querySelectorAll('.champions__icon');
     iconsAdded.forEach(el => {
-        el.remove();
-        
+        el.remove(); 
     })
-        
+
     addIcons();
 
+
     closePopup();
-    
+})
+
+document.addEventListener('click', e => {
+    if (e.target.classList.contains('champions__icon') && e.target.closest('.popup-champ__list')) {
+        e.target.classList.toggle('_current');
+    }
 })
 
 
-
 function addIcons() {
-    icons.forEach(el => {
-        if (el.classList.contains('_current')) {
-            let clone = el.cloneNode(true);
+    document.querySelector('.popup-champ__list').querySelectorAll('.champions__icon').forEach(el => {
+        let clone = el.cloneNode(true);
 
-            document.querySelector('.general-champ-list').appendChild(clone);
-        }
+        document.querySelector('.general-champ-list').appendChild(clone);
     })
 
     let listIcons = document.querySelector('.champions__list');
@@ -69,7 +67,16 @@ function addIcons() {
 function rmIcon(iconsAdded) {
     iconsAdded.forEach(el => {
         el.addEventListener('click', e => {
-            el.parentElement.removeChild(el);
+            el.classList.remove('_current');
+
+            document.querySelector('.popup-champ__list').querySelectorAll('.champions__icon').forEach(el => {
+                el.remove(); 
+            })
+            document.querySelector('.champions__list').querySelectorAll('.champions__icon').forEach(el => {
+                let clone = el.cloneNode(true);
+        
+                document.querySelector('.popup-champ__list-wrapper').appendChild(clone);
+            })
         })
     })
 }
@@ -81,4 +88,13 @@ function openPopup() {
 function closePopup() {
     popup.classList.remove('opened');
     document.querySelector("body").style.overflow = "visible";
+
+    document.querySelector('.popup-champ__list').querySelectorAll('.champions__icon').forEach(el => {
+        el.remove(); 
+    })
+    document.querySelector('.champions__list').querySelectorAll('.champions__icon').forEach(el => {
+        let clone = el.cloneNode(true);
+
+        document.querySelector('.popup-champ__list-wrapper').appendChild(clone);
+    })
 }
